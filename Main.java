14efdd2267747c2d13ff01297caa52583dedf71e
MatiasValencia <144842386+MatiasValencia@users.noreply.github.com>
2023-09-12 19:30:31 +0000

    cambios

---
+++ Main.java
@@ -0,0 +1,19 @@
+package java_com;
+
+public class Main
+{
+	public static void main(String[] args) {
+		String nombre;
+        String apellido;
+        String direccion;
+        String rut;
+        int saldo;
+
+        System.out.println("Bienvenido a Banco Javi Coins");
+        System.out.println("Ingrese su nombre");
+        System.out.println("Ingrese su apellido");
+        System.out.println("Ingrese su direccion");
+        System.out.println("Ingrese su rut");
+        System.out.println("Ingrese un saldo inicial para su cuenta");
+	}
+}
