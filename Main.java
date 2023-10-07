cf41ad93a747334483aa93a9f7e0dc50fcc4aa65
MatiasValencia <144842386+MatiasValencia@users.noreply.github.com>
2023-10-07 19:36:42 +0000

    change1

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 package java_com;
 
+import java.util.Scanner;
+
 public class Main
 {
 	public static void main(String[] args) {
@@ -9,8 +11,12 @@ public class Main
         String rut;
         int saldo;
 
+        Scanner scanner = new Scanner();
+
         System.out.println("Bienvenido a Banco Javi Coins");
         System.out.println("Ingrese su nombre");
+        nombre = scanner.nextLine();
+        System.out.println(nombre);
         System.out.println("Ingrese su apellido");
         System.out.println("Ingrese su direccion");
         System.out.println("Ingrese su rut");
